d9412cba5308391c9c3a6687c80668f84b263b20
alex <alexey.bazanov13@gmail.com>
2020-07-02 17:51:07 +0300

    Добавил www в домен сайта для мэйлера.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.active_storage.service = :amazon
 
-  config.action_mailer.default_url_options = { host: 'kurs-parikmaher.ru', port: 80 }
+  config.action_mailer.default_url_options = { host: 'www.kurs-parikmaher.ru', port: 80 }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
